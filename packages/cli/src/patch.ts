3c1463e3dd2d3adbe0ed55bd305bd05a4ff88a51
Max Murin <max@stainless.com>
2025-06-04 16:21:08 -0400

    lint, eslint ignore the file that has to do unreasonable things

--- packages/cli/src/patch.ts
+++ packages/cli/src/patch.ts
@@ -1,2 +1,4 @@
+/* eslint-disable */
+
 // @ts-ignore
-Object.assign(require("node-abort-controller"), {AbortController, AbortSignal});
+Object.assign(require('node-abort-controller'), { AbortController, AbortSignal });
